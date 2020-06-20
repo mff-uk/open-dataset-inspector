c481c6dbfc4b47811d19c148a7c58eb45ba04f6a
matejikj <jakub.matejik@centrum.cz>
2020-06-20 06:58:42 +0200

    text-wrapping

--- client/similarity-visualisation/visualisation/CircleVisualisation/CircleLabel.vue
+++ client/similarity-visualisation/visualisation/CircleVisualisation/CircleLabel.vue
@@ -1,15 +1,27 @@
 <template>
-    <text class="labels"
-        fill="white"
-        :x="labelData.x"
-        v-if="labelData.isLeaf && labelData.r > 5"
-        :font-size="labelData.r / 2"
-        :y="labelData.y"
-        dy=".35em"
-        @click.exact="emit(labelData)"
-        @click.ctrl="openWiki(labelData)"
-        >{{ labelData.label !== undefined ? labelData.label.length > 5 ? labelData.label.substring(0, 5) + ".." : labelData.label : labelData.id.substring(0, 5) + '.' }}
-     </text>
+  <text class="circle-labels"
+    fill="white"
+    :x="labelData.x"
+    v-if="labelData.isLeaf && labelData.r > 5"
+    :font-size="labelData.r > 60 ? 30 : labelData.r / 2"
+    :y="labelData.y"
+    dy=".35em"
+    @click.exact="emit(labelData)"
+    @click.ctrl="openWiki(labelData)"
+  >
+    <tspan v-if="labelData.label.length >= 18 && labelData.r < 120" :x="labelData.x" dy="-0.6em">{{ labelData.label.substring(0, 5) + "-" }}</tspan>
+    <tspan v-if="labelData.label.length >= 18 && labelData.r < 120" :x="labelData.x" dy="1em">{{ labelData.label.substring(5, 13) + "-" }}</tspan>
+    <tspan v-if="labelData.label.length >= 18 && labelData.r < 120" :x="labelData.x" dy="1em">{{ labelData.label.length > 18 ? labelData.label.substring(13, 18) + ".." : labelData.label.substring(13, 18) }}</tspan>
+
+    <tspan v-if="labelData.label.length >= 18 && labelData.r >= 120" :x="labelData.x" dy="0.3em">{{ labelData.label }}</tspan>
+
+    <tspan v-if="labelData.label.length < 18 && labelData.label.length > 6 && labelData.r < 80" :x="labelData.x" dy="0em">{{ labelData.label.substring(0, 6) + "-" }}</tspan>
+    <tspan v-if="labelData.label.length < 18 && labelData.label.length > 6 && labelData.r < 80" :x="labelData.x" dy="1em">{{ labelData.label.length > 12 ? labelData.label.substring(6, 12) + ".." : labelData.label.substring(6, 12) }}</tspan>
+
+    <tspan v-if="labelData.label.length < 18 && labelData.label.length > 6 && labelData.r >= 80" :x="labelData.x" dy="0.3em">{{ labelData.label }}</tspan>
+
+    <tspan v-if="labelData.label.length <= 6" :x="labelData.x" dy="0.3em">{{ labelData.label.substring(0, 7) }}</tspan>
+  </text>
 </template>
 
 <script lang="ts">
@@ -42,3 +54,11 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style>
+.circle-labels {
+  cursor: pointer;
+  text-anchor: middle;
+  pointer-events: none;
+}
+</style>
