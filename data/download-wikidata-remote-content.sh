ab8268cba5a1ff7ba5c433c19781dd00bde41e36
Petr Škoda <skodapetr@gmail.com>
2020-10-18 10:53:48 +0200

    Add data folder metadata

---
+++ data/download-wikidata-remote-content.sh
@@ -0,0 +1,3 @@
+wget https://dumps.wikimedia.org/other/wikidata/20181217.json.gz -O ./www.wikidata.org/20181217.json.gz
+wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.ttl.gz -O ./www.wikidata.org/latest-all.ttl.gz
+wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-lexemes.ttl.gz -O ./www.wikidata.org/latest-lexemes.ttl.gz
